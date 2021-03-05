b726d3da7813bc20e8fd6b62b05151fe181e2f13
ctlove0523 <478309639@qq.com>
2021-03-05 08:36:20 +0800

    device shadow

--- shadow.go
+++ shadow.go
@@ -9,10 +9,20 @@ type DeviceShadowData struct {
 	ServiceID string                 `json:"service_id"`
 	Desired   DeviceShadowProperties `json:"desired"`
 	Reported  DeviceShadowProperties `json:"reported"`
-	Version  int                `json:"version"`
+	Version   int                    `json:"version"`
 }
 
 type DeviceShadowProperties struct {
 	Properties interface{} `json:"properties"`
-	EventTime string `json:"event_time"`
+	EventTime  string      `json:"event_time"`
+}
+
+type UpdateDeviceShadowRequest struct {
+	Shadow []UpdateDeviceShadowDesired `json:"shadow"`
+}
+
+type UpdateDeviceShadowDesired struct {
+	ServiceId string      `json:"service_id"`
+	Desired   interface{} `json:"desired"`
+	Version   int         `json:"version"`
 }
